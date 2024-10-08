0cb83d6b8c9a6db79373e9727039606b302d861e
Alireza Khodayi <89078767+Alireza-Khodayi@users.noreply.github.com>
2024-10-08 15:24:52 +0330

    auto rotating globe , dragable and etc...

--- src/lib/components/Globe/Globe.svelte
+++ src/lib/components/Globe/Globe.svelte
@@ -1,10 +1,15 @@
 <script>
 	// @ts-nocheck
+	import { onMount } from 'svelte';
+	import { spring } from 'svelte/motion';
 
 	import * as topojson from 'topojson-client';
 	import { geoOrthographic, geoPath } from 'd3-geo';
 	import { scaleLinear } from 'd3-scale';
 	import { max } from 'd3-array';
+	import { timer } from 'd3-timer';
+	import { drag } from 'd3-drag';
+	import { select } from 'd3-selection';
 
 	import world from '$lib/data/world-110m.json';
 	import data from '$lib/data/countries-papulation.json';
@@ -19,7 +24,7 @@
 
 	$: projection = geoOrthographic()
 		.scale(width / 2)
-		.rotate([0, 0, 0])
+		.rotate([$xRotation, $yRotation, 0])
 		.translate([width / 2, height / 2]);
 
 	$: path = geoPath(projection);
@@ -27,16 +32,58 @@
 	const colorScale = scaleLinear()
 		.domain([0, max(data, (d) => d.population)]) //Input
 		.range(['#26362e', '#0dcc6c']); //Output
+
+	//Restructure countries array to include population from data
+	countries.forEach((country) => {
+		const metadata = data?.find((d) => d.id === country.id);
+		if (metadata) {
+			country.population = metadata.population;
+			country.country = metadata.country;
+		}
+	});
+
+	let xRotation = spring(0, { stiffness: 0.08, damping: 0.4 });
+	let yRotation = spring(0, { stiffness: 0.1, damping: 0.7 });
+
+	let degreesPerFrame = 0.8;
+
+	const t = timer((elapsed) => {
+		if (dragging) return;
+		$xRotation += degreesPerFrame;
+	}, 0);
+
+	let globe;
+
+	const DRAG_SENSITIVITY = 1;
+	let dragging = false;
+
+	onMount(() => {
+		const element = select(globe);
+		element.call(
+			drag()
+				.on('drag', (event) => {
+					$xRotation = $xRotation + event.dx * DRAG_SENSITIVITY;
+					$yRotation = $yRotation - event.dy * DRAG_SENSITIVITY;
+					dragging = true;
+				})
+				.on('end', () => {
+					dragging = false;
+				})
+		);
+	});
 </script>
 
-<div class="chart-container max-w-md mx-auto mt-20" bind:clientWidth={width}>
-	<svg {width} {height} class="overflow-visible">
+<div
+	class="chart-container max-w-md mx-auto mt-20 cursor-grab active:cursor-grabbing"
+	bind:clientWidth={width}
+>
+	<svg {width} {height} class="overflow-visible" bind:this={globe}>
 		<!-- Glow -->
 		<Glow />
 		<!-- Globe -->
 		<circle cx={width / 2} cy={height / 2} r={width / 2} fill="#1c1c1c" filter="url(#glow)" />
 		{#each countries as country}
-			<path d={path(country)} fill="#26362e" stroke="none" />
+			<path d={path(country)} fill={colorScale(country?.population || 0)} stroke="none" />
 		{/each}
 		<path d={path(borders)} fill="none" stroke="black" />
 	</svg>
